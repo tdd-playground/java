2d4a4c7c7b5705b88c059edf6eb8f45311340cac
David Bingham <dbinh@allstate.com>
2018-09-13 16:00:19 +0100

    Updating

---
+++ src/tdd/shoppingcart/ShoppingCart.java
@@ -0,0 +1,4 @@
+package tdd.shoppingcart;
+
+public class ShoppingCart {
+}
---
+++ test/tdd/shoppingcart/ShoppingCartTest.java
@@ -0,0 +1,35 @@
+package tdd.shoppingcart;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+
+public class ShoppingCartTest {
+
+    private ShoppingCart sc;
+
+    @BeforeEach
+    void setUp() {
+        sc = new ShoppingCart();
+    }
+
+    @Test
+    public void test() {
+    }
+
+    @Test
+    public void testa() {
+        for (int i = 0; i < array.length; i++) {
+             = array[i];
+            
+        }
+        for (Object o : ) {
+            
+        }
+        for (Iterator iterator = collection.iterator(); iterator.hasNext(); ) {
+            Object next =  iterator.next();
+            
+        }
+    }
+}
